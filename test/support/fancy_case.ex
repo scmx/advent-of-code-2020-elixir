a77d72abe2eec1a3ec4bf7cda40cb59a80d23172
Albert Arvidsson <albert.arvidsson@gmail.com>
2017-12-03 23:45:23 +0100

    Add Adventofcode.FancyCase ExUnit Case

---
+++ test/support/fancy_case.ex
@@ -0,0 +1,16 @@
+defmodule Adventofcode.FancyCase do
+  @moduledoc """
+  """
+
+  use ExUnit.CaseTemplate
+
+  using do
+    quote do
+      import Adventofcode.TestHelpers
+    end
+  end
+
+  setup _tags do
+    :ok
+  end
+end
