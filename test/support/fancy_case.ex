86c02dd7222c3723f76ff9bed8167f55b1981ab4
Albert Arvidsson <albert.arvidsson@gmail.com>
2017-12-03 23:51:10 +0100

    Move TestHelpers.with_puzzle_input into FancyCase

--- test/support/fancy_case.ex
+++ test/support/fancy_case.ex
@@ -6,11 +6,23 @@ defmodule Adventofcode.FancyCase do
 
   using do
     quote do
-      import Adventofcode.TestHelpers
+      import Adventofcode.FancyCase
     end
   end
 
   setup _tags do
     :ok
+  end
+
+  @doc """
+  Helper for running tests against puzzle input files.
+
+  Runs the given function with the contents of the file if it exists.
+  """
+  def with_puzzle_input(path, fun) do
+    case path |> File.read() do
+      {:ok, data} -> fun.(data |> String.trim())
+      {:error, _} -> nil
+    end
   end
 end
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,10 +1,1 @@
 ExUnit.start()
-
-defmodule Adventofcode.TestHelpers do
-  def with_puzzle_input(path, fun) do
-    case path |> File.read() do
-      {:ok, data} -> fun.(data |> String.trim())
-      {:error, _} -> nil
-    end
-  end
-end
