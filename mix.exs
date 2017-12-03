b85a8adf066f189456af93719a56be1b6b81456c
Albert Arvidsson <albert.arvidsson@gmail.com>
2017-12-03 23:35:47 +0100

    Add test/support to compilation paths during test

--- mix.exs
+++ mix.exs
@@ -6,6 +6,7 @@ defmodule Adventofcode.MixProject do
       app: :adventofcode,
       version: "0.1.0",
       elixir: "~> 1.7",
+      elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -25,4 +26,7 @@ defmodule Adventofcode.MixProject do
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
   end
+
+  def elixirc_paths(:test), do: ["lib", "test/support"]
+  def elixirc_paths(_), do: ["lib"]
 end
