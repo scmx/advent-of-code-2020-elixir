a378b38af730ee06f7da1dcd55b40bf77fbaafa4
Albert Arvidsson <albert.arvidsson@gmail.com>
2020-12-03 07:03:17 +0100

    Add Day03TobogganTrajectory.part_1/1

---
+++ lib/day_03_toboggan_trajectory.ex
@@ -0,0 +1,60 @@
+defmodule Adventofcode.Day03TobogganTrajectory do
+  use Adventofcode
+
+  require Logger
+
+  alias __MODULE__.Grid
+
+  def part_1(input) do
+    input
+    |> parse()
+    |> traverse(%{x: 3, y: 1})
+    |> Map.get(:result)
+  end
+
+  # def part_2(input) do
+  #   input
+  # end
+
+  def parse(input) do
+    input
+    |> String.trim()
+    |> String.split("\n")
+    |> Enum.map(&String.to_char_list/1)
+    |> Grid.new()
+  end
+
+  defmodule Grid do
+    @enforce_keys [:list, :size]
+    defstruct list: [], size: %{x: 0, y: 0}, pos: %{x: 0, y: 0}, result: 0
+
+    def new(list) do
+      %__MODULE__{list: list, size: %{x: list |> hd() |> length, y: list |> length()}}
+    end
+  end
+
+  def traverse(%Grid{pos: %{y: y}, size: %{y: height}} = grid, %{y: y_slope})
+      when y + y_slope > height,
+      do: grid
+
+  def traverse(grid, slope) do
+    grid
+    |> increment_result()
+    |> apply_slope(slope)
+    |> traverse(slope)
+  end
+
+  defp apply_slope(grid, slope) do
+    %{grid | pos: %{x: grid.pos.x + slope.x, y: grid.pos.y + slope.y}}
+  end
+
+  defp increment_result(grid) do
+    list_item = grid.list |> Enum.at(grid.pos.y)
+    x_index = rem(grid.pos.x, grid.size.x)
+
+    case list_item |> Enum.at(x_index) do
+      ?# -> %{grid | result: grid.result + 1}
+      ?. -> grid
+    end
+  end
+end
---
+++ test/day_03_toboggan_trajectory_test.exs
@@ -0,0 +1,49 @@
+defmodule Adventofcode.Day03TobogganTrajectoryTest do
+  use Adventofcode.FancyCase
+
+  import Adventofcode.Day03TobogganTrajectory
+
+  alias Adventofcode.Day03TobogganTrajectory.Grid
+
+  @example_input """
+  ..##.......
+  #...#...#..
+  .#....#..#.
+  ..#.#...#.#
+  .#...##..#.
+  ..#.##.....
+  .#.#.#....#
+  .#........#
+  #.##...#...
+  #...##....#
+  .#..#...#.#
+  """
+
+  describe "part_1/1" do
+    test "parse map into list, width, height" do
+      assert %Grid{
+               list: ['..##.......', '#...#...#..', '.#....#..#.' | _],
+               size: %{x: 11, y: 11}
+             } = @example_input |> parse()
+    end
+
+    test "slope right 3, down 1" do
+      grid = @example_input |> parse()
+      assert %Grid{result: 7} = grid |> traverse(%{x: 3, y: 1})
+    end
+
+    test_with_puzzle_input do
+      assert 191 = puzzle_input() |> part_1()
+    end
+  end
+
+  describe "part_2/1" do
+    # test "" do
+    #   assert 1337 = 1337 |> part_2()
+    # end
+
+    # test_with_puzzle_input do
+    #   assert 1337 = puzzle_input() |> part_2()
+    # end
+  end
+end
