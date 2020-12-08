00dad18aaf273ec0c2921896b501533f79953dc3
Albert Arvidsson <albert.arvidsson@gmail.com>
2020-12-09 00:25:06 +0100

    Refactor Day08HandheldHalting

--- lib/day_08_handheld_halting.ex
+++ lib/day_08_handheld_halting.ex
@@ -16,68 +16,30 @@ defmodule Adventofcode.Day08HandheldHalting do
     |> Parser.parse()
     |> State.new()
     |> Part2.run()
-    |> Enum.find(& &1.halt)
     |> State.accumulator()
   end
 
   defmodule State do
-    defstruct accumulator: 0,
-              instruction: 0,
-              program: %{},
-              history: MapSet.new(),
-              loop: false,
-              halt: false
+    defstruct accumulator: 0, index: 0, program: %{}, history: MapSet.new(), done: false
 
-    def new(%{} = program) do
-      %__MODULE__{program: program}
-    end
+    def new(%{} = program), do: %__MODULE__{program: program}
 
     def accumulator(%State{accumulator: accumulator}), do: accumulator
-
-    def accumulator(%State{} = state, change) do
-      %{state | accumulator: state.accumulator + change}
-    end
-
-    def instruction(%State{instruction: instruction, history: history} = state, offset) do
-      %{state | instruction: instruction + offset, history: MapSet.put(history, instruction)}
-    end
-
-    def acc(%State{} = state, argument) do
-      state
-      |> accumulator(argument)
-      |> instruction(+1)
-    end
-
-    def jmp(%State{} = state, offset) do
-      state
-      |> instruction(offset)
-    end
-
-    def nop(%State{} = state, _argument) do
-      state
-      |> instruction(+1)
-    end
-
-    def swap_operation(%State{program: program} = state, {instruction, {operation, argument}}) do
-      %{state | program: Map.put(program, instruction, {swap_operation(operation), argument})}
-    end
-
-    defp swap_operation(:jmp), do: :nop
-    defp swap_operation(:nop), do: :jmp
   end
 
   defmodule Part1 do
-    def run(%State{} = state) do
-      cond do
-        state.instruction in state.history ->
-          %{state | loop: true}
+    def run(%State{accumulator: acc} = state) do
+      if state.index in state.history do
+        %{state | done: :infinite_loop}
+      else
+        state = %{state | history: MapSet.put(state.history, state.index)}
 
-        !Map.has_key?(state.program, state.instruction) ->
-          %{state | halt: true}
-
-        {operation, argument} = Map.get(state.program, state.instruction) ->
-          apply(State, operation, [state, argument])
-          |> run()
+        case Map.get(state.program, state.index) do
+          nil -> %{state | done: :halt}
+          {:acc, amount} -> run(%{state | index: state.index + 1, accumulator: acc + amount})
+          {:jmp, offset} -> run(%{state | index: state.index + offset})
+          {:nop, _value} -> run(%{state | index: state.index + 1})
+        end
       end
     end
   end
@@ -85,31 +47,25 @@ defmodule Adventofcode.Day08HandheldHalting do
   defmodule Part2 do
     def run(%State{} = state) do
       state.program
-      |> Enum.filter(fn {_, {operation, _}} -> operation != :acc end)
-      |> Enum.map(&State.swap_operation(state, &1))
-      |> Enum.map(&Part1.run/1)
+      |> Enum.filter(fn {_, {op, _}} -> op != :acc end)
+      |> Enum.map(fn
+        {index, {:jmp, arg}} -> Map.put(state.program, index, {:nop, arg})
+        {index, {:nop, arg}} -> Map.put(state.program, index, {:jmp, arg})
+      end)
+      |> Enum.map(&Part1.run(%{state | program: &1}))
+      |> Enum.find(&(&1.done == :halt))
     end
   end
 
   defmodule Parser do
     def parse(input) do
-      input
-      |> String.trim()
-      |> String.split("\n")
-      |> Enum.map(&parse_instruction/1)
+      ~r/(\w{3}) (\+?-?\d+)/
+      |> Regex.scan(input)
       |> Enum.with_index()
-      |> Enum.map(fn {instruction, index} -> {index, instruction} end)
+      |> Enum.map(fn {[_, op, arg], index} ->
+        {index, {String.to_atom(op), String.to_integer(arg)}}
+      end)
       |> Enum.into(%{})
-    end
-
-    defp parse_instruction(line) do
-      line
-      |> String.split(" ")
-      |> do_parse_instruction
-    end
-
-    defp do_parse_instruction([operation, argument]) do
-      {String.to_atom(operation), String.to_integer(argument)}
     end
   end
 end
