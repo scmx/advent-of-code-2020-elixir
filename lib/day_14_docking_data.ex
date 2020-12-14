312a70492e323fc172e920b6da8a8fd97ece489a
Albert Arvidsson <albert.arvidsson@gmail.com>
2020-12-15 00:05:59 +0100

    Add Day14DockingData.part_2

--- lib/day_14_docking_data.ex
+++ lib/day_14_docking_data.ex
@@ -1,7 +1,7 @@
 defmodule Adventofcode.Day14DockingData do
   use Adventofcode
 
-  alias __MODULE__.{Part1, Utils}
+  alias __MODULE__.{Part1, Part2}
 
   def part_1(input) do
     input
@@ -9,9 +9,11 @@ defmodule Adventofcode.Day14DockingData do
     |> Part1.solve()
   end
 
-  # def part_2(input) do
-  #   input
-  # end
+  def part_2(input) do
+    input
+    |> parse
+    |> Part2.solve()
+  end
 
   defmodule Part1 do
     defstruct mask: String.duplicate("x", 36), mem: %{}
@@ -46,6 +48,66 @@ defmodule Adventofcode.Day14DockingData do
         {_, new} -> new
       end)
       |> Enum.join("")
+    end
+
+    defp to_bitmask(val), do: val |> Integer.to_string(2) |> String.pad_leading(36, "0")
+  end
+
+  defmodule Part2 do
+    defstruct mask: String.duplicate("x", 36), mem: %{}
+
+    def solve(instructions) when is_list(instructions) do
+      instructions
+      |> Enum.reduce(%__MODULE__{}, &solve/2)
+      |> sum
+    end
+
+    def solve({:mask, mask}, state) do
+      %{state | mask: mask}
+    end
+
+    def solve({:mem, [address, value]}, %__MODULE__{mask: mask} = state) do
+      address
+      |> apply_mask(mask)
+      |> Enum.map(&String.to_integer(&1, 2))
+      |> Enum.reduce(state, &%{&2 | mem: Map.put(&2.mem, &1, value)})
+    end
+
+    def sum(%__MODULE__{mem: mem}) do
+      mem
+      |> Map.values()
+      |> Enum.sum()
+    end
+
+    def apply_mask(num, mask) do
+      [to_bitmask(num), mask]
+      |> Enum.map(&String.graphemes/1)
+      |> Enum.zip()
+      |> Enum.map(fn
+        {current, "0"} -> current
+        {_, new} -> new
+      end)
+      |> apply_floating("")
+      |> Enum.map(&String.reverse/1)
+    end
+
+    def apply_floating(mask) when is_binary(mask) do
+      mask
+      |> String.graphemes()
+      |> apply_floating("")
+      |> Enum.map(&String.reverse/1)
+    end
+
+    def apply_floating([], result), do: [result]
+
+    def apply_floating(["X" | tail], result) do
+      ["0" <> result, "1" <> result]
+      |> Enum.flat_map(&apply_floating(tail, &1))
+    end
+
+    def apply_floating([b | tail], result) do
+      [b <> result]
+      |> Enum.flat_map(&apply_floating(tail, &1))
     end
 
     defp to_bitmask(val), do: val |> Integer.to_string(2) |> String.pad_leading(36, "0")
--- test/day_14_docking_data_test.exs
+++ test/day_14_docking_data_test.exs
@@ -3,6 +3,8 @@ defmodule Adventofcode.Day14DockingDataTest do
 
   import Adventofcode.Day14DockingData
 
+  alias Adventofcode.Day14DockingData.Part2
+
   describe "part_1/1" do
     @example """
     mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X
@@ -21,12 +23,42 @@ defmodule Adventofcode.Day14DockingDataTest do
   end
 
   describe "part_2/1" do
-    # test "" do
-    #   assert 1337 = 1337 |> part_2()
-    # end
+    @example """
+    mask = 000000000000000000000000000000X1001X
+    mem[42] = 100
+    mask = 00000000000000000000000000000000X0XX
+    mem[26] = 1
+    """
 
-    # test_with_puzzle_input do
-    #   assert 1337 = puzzle_input() |> part_2()
-    # end
+    test "in this example, the sum is 208." do
+      assert 208 = @example |> part_2()
+    end
+
+    test_with_puzzle_input do
+      assert 4_195_339_838_136 = puzzle_input() |> part_2()
+    end
+  end
+
+  describe "Part2.apply_floating" do
+    test "000" do
+      assert ["000"] = "000" |> Part2.apply_floating()
+    end
+
+    test "111" do
+      assert ["111"] = "111" |> Part2.apply_floating()
+    end
+
+    test "00X" do
+      assert ["000", "001"] = "00X" |> Part2.apply_floating()
+    end
+
+    test "X0X" do
+      assert ["000", "001", "100", "101"] = "X0X" |> Part2.apply_floating()
+    end
+
+    test "XXX" do
+      assert ["000", "001", "010", "011", "100", "101", "110", "111"] =
+               "XXX" |> Part2.apply_floating()
+    end
   end
 end
