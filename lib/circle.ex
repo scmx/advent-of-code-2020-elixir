d74b00625fcb37e4ec522cd161fb81992a4c9d9c
Albert Arvidsson <albert.arvidsson@gmail.com>
2020-12-24 09:39:33 +0100

    Extend Circle with more helpers

--- lib/circle.ex
+++ lib/circle.ex
@@ -9,8 +9,10 @@ defmodule Adventofcode.Circle do
   There are also meta-keys like size, current and counter that are kept up to
   date automatically as long as the built in API functions are used.
   """
-  def new do
-    %{size: 0, current: 0, counter: 0}
+  def new(values \\ []) do
+    values
+    |> Enum.reduce(%{size: 0, current: 0, counter: 0}, &insert_next(&2, &1))
+    |> move_next
   end
 
   def to_list(state) do
@@ -28,6 +30,14 @@ defmodule Adventofcode.Circle do
     do_to_list([value | list], state, next, last)
   end
 
+  def all(state) do
+    state
+    |> Map.delete(:size)
+    |> Map.delete(:current)
+    |> Map.delete(:counter)
+    |> Enum.map(fn {id, {_, val, _}} -> {id, val} end)
+  end
+
   def size(state) do
     state.size
   end
@@ -36,6 +46,11 @@ defmodule Adventofcode.Circle do
 
   def current(state) do
     elem(state[state.current], 1)
+  end
+
+  def at(state, id) do
+    {_, val, _} = state[id]
+    val
   end
 
   def move_next(state) do
@@ -50,10 +65,30 @@ defmodule Adventofcode.Circle do
     Map.put(state, :current, prev)
   end
 
+  def insert_after(state, values), do: insert_after(state, values, state.current)
+
+  def insert_after(state, values, after_id) when is_list(values) do
+    values
+    |> Enum.reduce({state, after_id}, fn value, {acc, prev} ->
+      id = acc.counter + 1
+      {_, _, next} = acc[prev]
+
+      {do_insert(acc, id, {prev, value, next}), id}
+    end)
+    |> elem(0)
+  end
+
+  def insert_next(state, values) when is_list(values) do
+    values
+    |> Enum.reduce(state, &insert_next(&2, &1))
+  end
+
   def insert_next(%{size: 0} = state, value) do
     id = state.counter + 1
 
-    do_insert_next(state, id, {id, value, id})
+    state
+    |> do_insert(id, {id, value, id})
+    |> Map.put(:current, id)
   end
 
   def insert_next(state, value) do
@@ -62,13 +97,14 @@ defmodule Adventofcode.Circle do
     prev = state.current
     {_, _, next} = state[state.current]
 
-    do_insert_next(state, id, {prev, value, next})
+    state
+    |> do_insert(id, {prev, value, next})
+    |> Map.put(:current, id)
   end
 
-  defp do_insert_next(state, id, {prev, _value, next} = item) do
+  defp do_insert(state, id, {prev, _value, next} = item) do
     state
     |> Map.put(id, item)
-    |> Map.put(:current, id)
     |> Map.update(prev, item, &put_elem(&1, 2, id))
     |> Map.update(next, item, &put_elem(&1, 0, id))
     |> Map.update(:size, 0, &(&1 + 1))
@@ -86,5 +122,14 @@ defmodule Adventofcode.Circle do
     |> Map.put(:current, next)
     |> Map.update(:size, 0, &(&1 - 1))
     |> Map.delete(state.current)
+  end
+
+  def take_after(state, amount) when is_integer(amount) and amount >= 1 do
+    {result, state} = Enum.reduce(1..amount, {[], move_next(state)}, &do_take/2)
+    {result, move_prev(state)}
+  end
+
+  defp do_take(_, {result, state}) do
+    {result ++ [current(state)], remove_current(state)}
   end
 end
